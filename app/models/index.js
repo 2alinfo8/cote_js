b8645f751dc79513dfd7010b23c9b9f0522571b3
2alinfo8 <79051536+2alinfo8@users.noreply.github.com>
2021-03-05 22:26:25 +0100

    Update index.js

--- app/models/index.js
+++ app/models/index.js
@@ -27,6 +27,7 @@ db.sequelize = sequelize;
 db.user = require("../models/user.model.js")(sequelize, Sequelize);
 db.role = require("../models/role.model.js")(sequelize, Sequelize);
 db.demande = require("../models/demande-accouplement.model.js")(sequelize, Sequelize);
+db.accessoire = require("../models/accessoire.model.js")(sequelize, Sequelize);
 
 db.role.belongsToMany(db.user, {
   through: "user_roles",
